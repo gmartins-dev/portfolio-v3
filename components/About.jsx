b37cf82ee0fdb0122a84ab33d150914deadb8ef0
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-12-31 10:52:37 +0000

    css updates

--- components/About.jsx
+++ components/About.jsx
@@ -13,23 +13,26 @@ const About = () => {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 ">
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            My focus is on Frontend Development on Web and Mobile applications,
+            I have experience with projects using React, React Native, Next.js,
+            Angular, Vue, JavaScript, TypeScript, HTML, CSS, Styled-Components,
+            TailwindCSS, Bootstrap, Chakra UI, Material UI and others.
           </p>
           <p className="py-2 ">
-            I started web developement in 2013 managing multiple e-commerce
-            websites on CMS platforms such as WordPress, BigCommerce, and
-            Shopify. I have experience working directly with clients and taking
-            mock wireframes all the way to deployed applications. In my spare
-            time I run Code Commerce, a Youtube channel where I teach web
-            developement and various front-end technologies.
+            What most attracts me about working with frontend development is the
+            possibility of combining both the creative part to think of
+            solutions that directly affect the way people will use websites,
+            programs, and mobile applications together with the logical part of
+            solving problems so that this is done in the most optimized and
+            performative way possible.
           </p>
+          <p className="py-2 ">
+            I'm constantly looking to improve as a professional and learn on
+            demand in new challenges, so I'm always open to opportunities where
+            I need to learn to work with any software development tools that can
+            help me solve problems in the best possible way.
+          </p>
+
           <Link href="/#projects">
             <p className="py-2 underline cursor-pointer">
               Check out some of my latest projects.
