c1062366ded83f047f06c458c00094255cc1b4d2
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-12-31 07:37:13 +0000

    tests

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import Main from "../components/Main"
 import Projects from "../components/Projects"
 import Skills from "../components/Skills"
 
-
 export default function Home() {
   return (
     <div>
@@ -16,7 +15,7 @@ export default function Home() {
           name="description"
           content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences."
         />
-        <link rel="icon" href="/fav.png" />
+        <link rel="icon" href="/public/assets/favicon.svg" />
       </Head>
 
       <Main />
